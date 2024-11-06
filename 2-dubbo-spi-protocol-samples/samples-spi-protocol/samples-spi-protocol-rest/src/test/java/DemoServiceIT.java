dd6157f8304bd0d48700efb8702ad4ce40c264d7
Young <zeruiyoung@gmail.com>
2024-11-06 17:49:15 +0800

    feat: add spi-protocol-rest-samples

---
+++ 2-dubbo-spi-protocol-samples/samples-spi-protocol/samples-spi-protocol-rest/src/test/java/DemoServiceIT.java
@@ -0,0 +1,54 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"classpath:/spring/http-consumer.xml"})
+public class DemoServiceIT {
+
+    @Test
+    public void test() throws Exception {
+        try (CloseableHttpClient httpClient = HttpClients.createDefault()) {
+            HttpPost request = new HttpPost("http://localhost:8081/demo/sayHello");
+            request.setHeader("Content-Type", "application/json");
+            String json = "world";
+            StringEntity entity = new StringEntity(json);
+            request.setEntity(entity);
+
+            try (CloseableHttpResponse response = httpClient.execute(request)) {
+                HttpEntity responseEntity = response.getEntity();
+                if (responseEntity != null) {
+                    String result = EntityUtils.toString(responseEntity);
+                    Assert.assertTrue(result.startsWith("Hello world"));
+                }
+            }
+        }
+    }
+}
