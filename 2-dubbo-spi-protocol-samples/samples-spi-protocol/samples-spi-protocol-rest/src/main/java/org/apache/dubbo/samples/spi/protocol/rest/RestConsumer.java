c4e29862a62fc37b9fc4bd97a3bc690ad67fc268
Young <zeruiyoung@gmail.com>
2024-11-06 17:49:15 +0800

    feat: add spi-protocol-rest-samples

---
+++ 2-dubbo-spi-protocol-samples/samples-spi-protocol/samples-spi-protocol-rest/src/main/java/org/apache/dubbo/samples/spi/protocol/rest/RestConsumer.java
@@ -0,0 +1,53 @@
+/*
+ *
+ *   Licensed to the Apache Software Foundation (ASF) under one or more
+ *   contributor license agreements.  See the NOTICE file distributed with
+ *   this work for additional information regarding copyright ownership.
+ *   The ASF licenses this file to You under the Apache License, Version 2.0
+ *   (the "License"); you may not use this file except in compliance with
+ *   the License.  You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ *
+ */
+
+package org.apache.dubbo.samples.spi.protocol.rest;
+
+
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+
+public class RestConsumer {
+
+    public static void main(String[] args) throws Exception {
+        CloseableHttpClient httpClient = HttpClients.createDefault();
+        try {
+            HttpPost request = new HttpPost("http://localhost:8081/demo/sayHello");
+            request.setHeader("Content-Type", "application/json");
+            String json = "world";
+            StringEntity entity = new StringEntity(json);
+            request.setEntity(entity);
+
+            try (CloseableHttpResponse response = httpClient.execute(request)) {
+                HttpEntity responseEntity = response.getEntity();
+                if (responseEntity != null) {
+                    String result = EntityUtils.toString(responseEntity);
+                    System.out.println(result);
+                }
+            }
+        } finally {
+            httpClient.close();
+        }
+    }
+}
